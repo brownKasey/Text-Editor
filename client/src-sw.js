8073edb9e708e1bb9ffb01e236590b2105939665
unknown <kaseybrown28@icloud.com>
2023-09-21 19:07:34 -0400

    deployment

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst } = require("workbox-strategies");
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
 const { registerRoute } = require("workbox-routing");
 const { CacheableResponsePlugin } = require("workbox-cacheable-response");
 const { ExpirationPlugin } = require("workbox-expiration");
@@ -24,8 +24,6 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === "navigate", pageCache);
-
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
@@ -37,3 +35,5 @@ registerRoute(
     ],
   })
 );
+
+registerRoute();
