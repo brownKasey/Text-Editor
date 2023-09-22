e85ab80eafe6f2cb0418941ee43b5fd4fccfc871
brownKasey <130613198+brownKasey@users.noreply.github.com>
2023-09-22 15:07:31 -0400

    Update database.js to display the save data on the dom
    line 52

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -49,7 +49,7 @@ export const getDb = async () => {
   //confirmation of the request
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result.value;
 };
 
 initdb();
